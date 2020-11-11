2e85648566ffc4080ea5fab8b05aefff083cff31
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-11-11 12:44:35 +0900

    Create 보석 쇼핑.py

---
+++ Programmers/2020 카카오 인턴십/보석 쇼핑.py
@@ -0,0 +1,34 @@
+def solution(gems) :
+    n = len(set(gems))
+    answer = [0, len(gems)-1]
+    start = 0
+    end = 0
+    dic = {gems[0] : 1}
+    while start < len(gems) and end < len(gems) :
+        if len(dic) == n :
+            if answer[1] - answer[0] > end - start :
+                answer[0] = start
+                answer[1] = end
+                pass
+            if dic[gems[start]] == 1 :
+                del(dic[gems[start]])
+            else :
+                dic[gems[start]] -= 1
+                pass
+            start += 1
+        else : 
+            end += 1
+            if end == len(gems) :
+                break
+            else :
+                if dic.get(gems[end]) is None :
+                    dic[gems[end]] = 1
+                else :
+                    dic[gems[end]] += 1
+                    pass
+                pass
+            pass
+        pass
+    answer[0] += 1
+    answer[1] += 1
+    return answer
