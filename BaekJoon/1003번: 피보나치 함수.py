bcf558dcbd70706d6baf623abc461cec505fa185
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-04 23:58:48 +0900

    Update 1003번: 피보나치 함수.py

--- BaekJoon/1003번: 피보나치 함수.py
+++ BaekJoon/1003번: 피보나치 함수.py
@@ -1,1 +1,12 @@
-ttt
+def solution(num) :
+    lst = [(1,0), (0,1)]
+    for i in range(num-1) :
+        cnt0 = lst[i][0] + lst[i+1][0]
+        cnt1 = lst[i][1] + lst[i+1][1]
+        lst.append((cnt0, cnt1))
+    print(lst[num][0], lst[num][1])
+
+T = int(input())
+for i in range(T) :
+    N = int(input())
+    solution(N)
