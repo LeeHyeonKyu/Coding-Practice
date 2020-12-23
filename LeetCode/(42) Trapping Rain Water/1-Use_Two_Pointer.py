6cf747a5d7679496c407654cef47df5386530f38
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-24 00:28:54 +0900

    Create 1-Use_Two_Pointer.py

---
+++ LeetCode/(42) Trapping Rain Water/1-Use_Two_Pointer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height) -> int:
+        if not height :
+            return 0
+        
+        volume = 0
+        left = 0
+        right = len(height)-1
+        left_max = height[left]
+        right_max = height[right]
+        
+        while left < right :
+            left_max = max(height[left], left_max)
+            right_max = max(height[right], right_max)
+            
+            if left_max <= right_max :
+                volume += left_max - height[left]
+                left += 1
+            else :
+                volume += right_max - height[right]
+                right -= 1
+            
+        return volume
+
+'''
+Runtime : 52 ms
+Memory : 14.9 MB
+'''
