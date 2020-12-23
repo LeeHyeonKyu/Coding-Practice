1d16a50a1e2ce3fa1430608677e0c3d5a659c4a8
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-23 22:02:46 +0900

    Delete (1) Two Sum.py

--- LeetCode/(1) Two Sum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i_idx, i in enumerate(nums[:-1]) :
-            for j_idx, j in enumerate(nums[i_idx+1:]) :
-                if i + j == target :
-                    return [i_idx, i_idx + j_idx + 1]
