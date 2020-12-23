af98a5b3ec2af01f3f95b998c15b12b073f1fb13
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-23 22:31:33 +0900

    Create 1-Use_Sort.py

---
+++ LeetCode/(937) Reorder Log Files/1-Use_Sort.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        let_lst = []
+        dig_lst = []
+        
+        for log in logs :
+            if log.split()[1].isalpha() :
+                let_lst.append(log)
+            else : 
+                dig_lst.append(log)
+        
+        let_lst.sort(key=lambda x : (x.split()[1:], x.split()[0]))
+        
+        return let_lst + dig_lst
