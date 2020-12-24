374ef650dc8262eebb8d6be668cdc80b153df79c
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-24 14:08:02 +0900

    Create 2-Use_Kadane's_Algorithm.py

---
+++ LeetCode/(121) Best Time to Buy and Sell Stock/2-Use_Kadane's_Algorithm.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = sys.maxsize
+        
+        # 최소값과 최대값을 갱신
+        for price in prices :
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+        
+        return profit
+
+'''
+Runtime : 68 ms
+Memory : 15 MB
+'''
