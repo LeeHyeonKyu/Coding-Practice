c5919c35493f97e26ae4955bc538ed1cea10bfac
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2020-12-30 08:25:11 +0900

    Create 3-Use_DFS.py

---
+++ LeetCode/(938) Range Sum of BST/3-Use_DFS.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        if not root :
+            return 0
+        else :
+            return (root.val if low <= root.val <= high else 0) + \
+                    self.rangeSumBST(root.left, low, high) + \
+                    self.rangeSumBST(root.right, low, high)
+
+'''
+Runtime : 268 ms
+Memory : 22.3 MB
+'''
