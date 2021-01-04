8cf3aca5d83d6a4d4b44d84b809c99b9b318974c
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2021-01-04 18:38:35 +0900

    Create 1-Use_Memoization.py

---
+++ LeetCode/(56) Maximum Subarray/1-Use_Memoization.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sums = [nums[0]]
+        for i in range(1, len(nums)) :
+            sums.append(nums[i] + (sums[i-1] if sums[i-1] > 0 else 0))
+        return max(sums)
+
+'''
+Runtime : 64 ms
+Memory : 15.2 MB
+'''
