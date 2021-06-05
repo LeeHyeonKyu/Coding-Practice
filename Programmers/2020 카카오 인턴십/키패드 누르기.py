ffe35c3eb4bb2060b69ad89f59ba1081c2ffe49e
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2021-06-05 17:23:41 +0900

    Update 키패드 누르기.py

--- Programmers/2020 카카오 인턴십/키패드 누르기.py
+++ Programmers/2020 카카오 인턴십/키패드 누르기.py
@@ -1,78 +1,24 @@
-def solution(numbers:list, hand:str) -> str:
-    L = '*'
-    R = '#'
-    answer_lst = []
-
-    dict2 = {1:1, 2:0, 3:1, 4:2, 5:1, 6:2, 7:3, 8:2, 9:3, '*':4, 0:3, '#':4}
-    dict5 = {1:2, 2:1, 3:2, 4:1, 5:0, 6:1, 7:2, 8:1, 9:2, '*':3, 0:2, '#':3}
-    dict8 = {1:3, 2:2, 3:3, 4:2, 5:1, 6:2, 7:1, 8:0, 9:1, '*':2, 0:1, '#':2}
-    dict0 = {1:4, 2:3, 3:4, 4:3, 5:2, 6:3, 7:2, 8:1, 9:2, '*':1, 0:0, '#':1}
-
+def solution(numbers, hand):
+    answer = ''
+    numbers = list(map(lambda x : x-1 if x != 0 else 10, numbers))
+    L = (3, 0)
+    R = (3, 2)
     for num in numbers :
-        if num in (1, 4, 7, '*') :
-            answer_lst.append('L')
-            L = num
-        elif num in (3, 6, 9, '#') :
-            answer_lst.append('R')
-            R = num
-        elif num in (2, 5, 8, 0) :
-            if num == 2 :
-                if dict2[R] > dict2[L] :
-                    answer_lst.append('L')
-                    L = num
-                elif dict2[R] < dict2[L] :
-                    answer_lst.append('R')
-                    R = num
-                elif dict2[R] == dict2[L] :
-                    if hand == 'left' :
-                        answer_lst.append('L')
-                        L = num
-                    else :
-                        answer_lst.append('R')
-                        R = num
-            if num == 5 :
-                if dict5[R] > dict5[L] :
-                    answer_lst.append('L')
-                    L = num
-                elif dict5[R] < dict5[L] :
-                    answer_lst.append('R')
-                    R = num
-                elif dict5[R] == dict5[L] :
-                    if hand == 'left' :
-                        answer_lst.append('L')
-                        L = num
-                    else :
-                        answer_lst.append('R')
-                        R = num
-            if num == 8 :
-                if dict8[R] > dict8[L] :
-                    answer_lst.append('L')
-                    L = num
-                elif dict8[R] < dict8[L] :
-                    answer_lst.append('R')
-                    R = num
-                elif dict8[R] == dict8[L] :
-                    if hand == 'left' :
-                        answer_lst.append('L')
-                        L = num
-                    else :
-                        answer_lst.append('R')
-                        R = num
-            if num == 0 :
-                if dict0[R] > dict0[L] :
-                    answer_lst.append('L')
-                    L = num
-                elif dict0[R] < dict0[L] :
-                    answer_lst.append('R')
-                    R = num
-                elif dict0[R] == dict0[L] :
-                    if hand == 'left' :
-                        answer_lst.append('L')
-                        L = num
-                    else :
-                        answer_lst.append('R')
-                        R = num
-
-    answer = ''.join(answer_lst)
-
+        tmp = divmod(num, 3)
+        if tmp[1] == 0 :
+            answer += 'L'
+            L = tmp
+        elif tmp[1] == 2 :
+            answer += 'R'
+            R = tmp
+        else :
+            L_dist = abs(L[0]-tmp[0]) + abs(L[1]-tmp[1])
+            R_dist = abs(R[0]-tmp[0]) + abs(R[1]-tmp[1])
+            if (L_dist == R_dist and hand == 'right') or L_dist > R_dist : 
+                answer += 'R'
+                R = tmp
+            elif (L_dist == R_dist and hand == 'left') or L_dist < R_dist : 
+                answer += 'L'
+                L = tmp
+        
     return answer
