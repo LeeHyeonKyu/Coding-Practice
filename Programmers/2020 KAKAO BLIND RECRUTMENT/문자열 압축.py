57d43ce9cf4f8ac38cc389cf052e873425f231f9
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2021-06-12 20:55:03 +0900

    Update 문자열 압축.py

--- Programmers/2020 KAKAO BLIND RECRUTMENT/문자열 압축.py
+++ Programmers/2020 KAKAO BLIND RECRUTMENT/문자열 압축.py
@@ -1,26 +1,22 @@
-def solution(s) :
+def solution(s):
     tot_len = len(s)
     answer_lst = []
-
+    
     for window_size in range(1, (tot_len//2)+2) :
         start_point = 0
-        continue_cnt = 0
+        duplicate_cnt = 0
         window_result = tot_len
-
-        while True :
-            if start_point + window_size > tot_len :
-                answer_lst.append(window_result)
-                break # break point
+        
+        while start_point + window_size <= tot_len :
             front = s[start_point : start_point + window_size]
             back = s[start_point + window_size : start_point + (2 * window_size)]
             if front == back :
-                continue_cnt += 1
-            elif continue_cnt >= 1 :
-                window_result = window_result - (continue_cnt * window_size) + len(str(continue_cnt+1))
-                continue_cnt = 0
-                pass # end if-elif
+                duplicate_cnt += 1
+            elif duplicate_cnt >= 1 :
+                window_result = window_result - (duplicate_cnt * window_size) + len(str(duplicate_cnt+1))
+                duplicate_cnt = 0
             start_point += window_size
-            pass # end while
-        pass # end all loop
+        
+        answer_lst.append(window_result)
     
     return min(answer_lst)
