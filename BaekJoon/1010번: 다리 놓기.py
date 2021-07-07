84d73cd89a668566bba6fd3dcf0636ed8e46c261
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2021-07-08 00:48:13 +0900

    Update 1010번: 다리 놓기.py

--- BaekJoon/1010번: 다리 놓기.py
+++ BaekJoon/1010번: 다리 놓기.py
@@ -1,1 +1,15 @@
+import sys
 
+def main(N: int, M: int) -> int:
+    answer = 1
+    for i in range(M, M-N, -1):
+        answer *= i
+    for i in range(N, 1, -1):
+        answer //= i
+    return int(answer)
+
+input = sys.stdin.readline
+k = int(input())
+for _ in range(k):
+    N, M = list(map(int, input().split()))
+    print(main(N, M))
