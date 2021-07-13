b96417bcae23afeb1ae6b19d3c259ce270957b18
think_wisely <hklee128@naver.com>
2021-07-13 22:41:45 +0900

    Upload Files

--- BaekJoon/친구_네트워크(4195).py
+++ BaekJoon/친구_네트워크(4195).py
@@ -26,3 +26,41 @@ for _ in range(num_test_cases):
     for _ in range(num_input):
         name1, name2 = input().split()
         print(main(name1, name2))
+
+
+
+
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        p = find(parent[x])
+        parent[x] = p
+        return parent[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x != y:
+        parent[y] = x
+        number[x] += number[y]
+
+test_case = int(input())
+
+for _ in range(test_case):
+    parent = dict()
+    number = dict ()
+    f = int(input())
+    for _ in range(f):
+        x, y = input().split()
+        if x not in parent:
+            parent[x] = x
+            number[x] = 1
+        if y not in parent:
+            parent[y] = y
+            number[y] = 1
+        
+        union(x, y)
+
+        print(number[find(x)])
