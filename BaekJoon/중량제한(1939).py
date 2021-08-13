af46561a74b7a765a19e79bfb9d7d12303966fa6
think_wisely <hklee128@naver.com>
2021-08-13 21:26:45 +0900

    Upload Files

--- BaekJoon/중량제한(1939).py
+++ BaekJoon/중량제한(1939).py
@@ -1,12 +1,41 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-def main(N):
-    return
+n, m = map(int, input().split())
+adj = [[] for _ in range(n + 1)]
 
-N, M = list(map(int, input().split()))
-for _ in range(M):
-    land1, land2, weight = list(map(int, input().split()))
-    pass
-target1, taget2 = list(map(int, input().split()))
-print(main(N))
+def bfs(c):
+    queue = deque([start_node])
+    visited = [False] * (n + 1)
+    visited[start_node] = True
+
+    while queue:
+        x = queue.popleft()
+        for y, weight in adj[x]:
+            if not visited[y] and weight >= c:
+                visited[y] = True
+                queue.append(y)
+
+    return visited[end_node]
+
+start = 1000000000
+end = 1
+
+for _ in range(m):
+    x, y, weight = map(int, input().split())
+    adj[x].append((y, weight))
+    adj[y].append((x, weight))
+    start = min(start, weight)
+    end = max(end, weight)
+
+start_node, end_node = map(int, input().split())
+
+result = start
+while(start <= end):
+    mid = (start + end) // 2 # mid는 현재의 중량을 의미합니다.
+    if bfs(mid): # 이동이 가능하므로, 중량을 증가시킵니다.
+        result = mid
+        start = mid + 1
+    else: # 이동이 불가능하므로, 중량을 감소시킵니다.
+        end = mid - 1
+
+print(result)
