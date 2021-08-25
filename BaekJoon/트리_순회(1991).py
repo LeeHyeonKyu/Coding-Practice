44a0d946f7fbb9a5f32c05430fc254ff1e379694
think_wisely <hklee128@naver.com>
2021-08-25 20:57:12 +0900

    Upload Files

--- BaekJoon/트리_순회(1991).py
+++ BaekJoon/트리_순회(1991).py
@@ -1,66 +1,39 @@
-from collections import defaultdict
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
 
-class Node():
-    def __init__(self, name):
-        self.name = name
-        self.left = None
-        self.right = None
+def pre_order(node):
+    print(node.data, end='')
+    if node.left_node != '.':
+        pre_order(tree[node.left_node])
+    if node.right_node != '.':
+        pre_order(tree[node.right_node])
 
-class Tree():
-    def __init__(self):
-        self.nodes = defaultdict(str)
+def in_order(node):
+    if node.left_node != '.':
+        in_order(tree[node.left_node])
+    print(node.data, end='')
+    if node.right_node != '.':
+        in_order(tree[node.right_node])
 
-def str_to_node(char):
-    if char not in tree.nodes:
-        node = Node(char)
-        tree.nodes[char] = node
-    else:
-        node = tree.nodes[char]
-    return node
+def post_order(node):
+    if node.left_node != '.':
+        post_order(tree[node.left_node])
+    if node.right_node != '.':
+        post_order(tree[node.right_node])
+    print(node.data, end='')
 
-tree = Tree()
-N = int(input())
-for i in range(N):
-    P, L, R =  input().split()
-    p_node = str_to_node(P)
-    l_node = str_to_node(L)
-    r_node = str_to_node(R)
-    p_node.left = l_node
-    p_node_right = r_node
+n = int(input())
+tree = {}
 
-def preorder(node, lst):
-    if node.name == '.':
-        pass
-    else:
-        lst.append(node.name)
-        if node.left is not None:
-            preorder(node.left, lst)
-        if node.right is not None:
-            preorder(node.right, lst)
-    return ''.join(lst)
+for i in range(n):
+    data, left_node, right_node = input().split()
+    tree[data] = Node(data, left_node, right_node)
 
-def inorder(node, lst):
-    if node.name == '.':
-        pass
-    else:
-        if node.left is not None:
-            inorder(node.left, lst)
-        lst.append(node.name)
-        if node.right is not None:
-            inorder(node.right, lst)
-    return ''.join(lst)
-
-def postorder(node, lst):
-    if node.name == '.':
-        pass
-    else:
-        if node.left is not None:
-            postorder(node.left, lst)
-        if node.right is not None:
-            postorder(node.right, lst)
-        lst.append(node.name)
-    return ''.join(lst)
-    
-print(preorder(tree.nodes['A'], []))
-print(inorder(tree.nodes['A'], []))
-print(postorder(tree.nodes['A'], []))
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
