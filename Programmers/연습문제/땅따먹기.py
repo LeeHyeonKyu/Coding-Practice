bfe5a0bf94ad5ba521d115092c7451564f21de57
think_wisely <hklee128@naver.com>
2022-01-02 19:23:12 +0900

    Upload Files

---
+++ Programmers/연습문제/땅따먹기.py
@@ -0,0 +1,12 @@
+def solution(land):
+    answer = 0
+    stack = [(0, 0, 5)]
+    while stack:
+        val, row_idx, prev_num_idx = stack.pop()
+        if row_idx == len(land):
+            answer = max(answer, val)
+        else:
+            for num_idx, num in enumerate(land[row_idx]):
+                if prev_num_idx != num_idx:
+                    stack.append((val+num, row_idx+1, num_idx))
+    return answer
