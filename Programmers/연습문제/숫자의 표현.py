df47b6dcc0a05e12f02569644c63f764f7ade7d1
think_wisely <hklee128@naver.com>
2022-01-08 23:05:24 +0900

    Upload Files

--- Programmers/연습문제/숫자의 표현.py
+++ Programmers/연습문제/숫자의 표현.py
@@ -1,12 +1,8 @@
 def solution(n):
     answer = 0
-    lst = range(1, n+1)
-    for window_size in lst:
-        i = 0
-        while i + window_size <= n:
-            if sum(lst[i:i+window_size]) == n:
-                print(lst[i:i+window_size])
-                answer += 1
-                break
-            i += 1
+    for i in range(1, n):
+        if i % 2 == 0 and (n-1) % i == 0 and sum(range(1, i+1)) <= n-1:
+            answer += 1
+        elif i % 2 == 1 and n % i == 0 and sum(range(1, i+1)) <= n:
+            answer += 1
     return answer
