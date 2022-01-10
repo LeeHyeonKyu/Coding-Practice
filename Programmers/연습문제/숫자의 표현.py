7a9d06051185e1e5c0867eb9a68bb81cb2c13e0c
think_wisely <hklee128@naver.com>
2022-01-10 21:00:11 +0900

    Upload Files

--- Programmers/연습문제/숫자의 표현.py
+++ Programmers/연습문제/숫자의 표현.py
@@ -1,9 +1,7 @@
 def solution(n):
     answer = 0
     for i in range(1, n):
-        if i % 2 == 0 and (n-1) % i == 0 and sum(range(1, i+1)) <= n-1:
+        x = (n - sum(range(i))) / i
+        if x == int(x) and x > 0:
             answer += 1
-        elif i % 2 == 1 and n % i == 0 and sum(range(1, i+1)) <= n:
-            answer += 1
-            
     return answer
