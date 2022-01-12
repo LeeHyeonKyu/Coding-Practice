1f7e3449320f2a8396d12d14f865bc58aee5e7fe
think_wisely <hklee128@naver.com>
2022-01-12 22:42:34 +0900

    Upload Files

--- Programmers/연습문제/숫자의 표현.py
+++ Programmers/연습문제/숫자의 표현.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = 0
     for i in range(1, n//2 + 2):
-        x = (n - sum(range(i))) / i
+        x = (n - (i*(i-1))/2) / i
         if x == int(x) and x > 0:
             answer += 1
     return answer
