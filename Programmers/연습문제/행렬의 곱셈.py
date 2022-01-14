0eff639a2f42d55ccfe299e1a74f5ae0ad46477b
think_wisely <hklee128@naver.com>
2022-01-14 20:43:22 +0900

    Upload Files

--- Programmers/연습문제/행렬의 곱셈.py
+++ Programmers/연습문제/행렬의 곱셈.py
@@ -2,10 +2,10 @@ def solution(arr1, arr2):
     r_limit = len(arr1)
     c_limit = len(arr1[0])
     result = []
-    for l in range(l_limit):
+    for r in range(r_limit):
         tmp_lst = []
         tmp = 0
-        for c in range(l_limit):
-            tmp += arr1[l][c] * arr2[c][l]
+        for c in range(c_limit):
+            tmp += arr1[r][c] * arr2[c][r]
         tmp_lst.append(tmp)
     return result
