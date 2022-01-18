4c89816bf8c89d78bf2eec3683bc527ad4fb94eb
think_wisely <hklee128@naver.com>
2022-01-18 20:54:30 +0900

    Upload Files

---
+++ Programmers/Summer Winter Coding(2019)/멀쩡한 사각형.py
@@ -0,0 +1,11 @@
+from math import ceil
+
+def solution(w, h):
+    x, y, cnt, r = 0, 0, 0, -1
+    while r != 0:
+        x += 1
+        tmp = (h/w)*x
+        cnt += ceil(tmp-y)
+        y = tmp
+        q, r = divmod(h*x, w)
+    return (w*h)-(cnt*(h/q))
