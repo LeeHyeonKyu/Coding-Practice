abc1fa7cccf731b6e08781734458308642f4b152
think_wisely <hklee128@naver.com>
2022-01-19 19:25:27 +0900

    Upload Files

--- Programmers/Summer Winter Coding(2019)/멀쩡한 사각형.py
+++ Programmers/Summer Winter Coding(2019)/멀쩡한 사각형.py
@@ -5,7 +5,7 @@ def solution(w, h):
     while r != 0:
         x += 1
         tmp = (h/w)*x
-        cnt += ceil(tmp-y)
+        cnt += ceil(tmp-int(y))
         y = tmp
         q, r = divmod(h*x, w)
     return (w*h)-(cnt*(h/q))
