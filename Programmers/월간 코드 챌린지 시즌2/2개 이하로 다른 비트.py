fbad6c206d7d9a15d89aaee3703c1c07e3ff5657
think_wisely <hklee128@naver.com>
2022-01-27 23:01:16 +0900

    Upload Files

--- Programmers/월간 코드 챌린지 시즌2/2개 이하로 다른 비트.py
+++ Programmers/월간 코드 챌린지 시즌2/2개 이하로 다른 비트.py
@@ -1,11 +1,8 @@
 def bit_op(num):
     bit = bin(num)[2:]
-    if bit[-1] == '0':
-        return num+1
-    elif bit[-2] == '0':
-        return num+1
-    elif bit[-3] == '0':
-        return num+1
+    for i in range(1, min(3, len(bit))+1):
+        if bit[-i] == '0':
+            return num+1
     
     cnt = 0
     for i in bit[::-1]:
@@ -13,8 +10,9 @@ def bit_op(num):
             cnt +=1
         else:
             break
-    result = bin(num+1)
-    if cnt > 3:
+    result = bin(num+1)[2:]
+    
+    if len(result) == len(bit):
         result = result[:-cnt] + '00' + ('1' * (cnt-2))
     else:
         result = result[:-cnt] + '0' + ('1' * (cnt-1))
