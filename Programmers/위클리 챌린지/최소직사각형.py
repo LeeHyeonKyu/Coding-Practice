0d9825c0f0aa6c75f95cbe40f1f8a9e7cce3d0bf
think_wisely <72373733+LeeHyeonKyu@users.noreply.github.com>
2022-02-01 18:44:24 +0900

    Upload Files

--- Programmers/위클리 챌린지/최소직사각형.py
+++ Programmers/위클리 챌린지/최소직사각형.py
@@ -3,6 +3,7 @@ def calc_max(base, new):
     tmp2 = max(base[0], new[1]), max(base[1], new[0])
     return tmp1 if tmp1[0] * tmp1[1] < tmp2[0] * tmp2[1] else tmp2
 
+
 def solution(sizes):
     result = [(0,0), (0,0)]
     for t1, t2 in sizes:
